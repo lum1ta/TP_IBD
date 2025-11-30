557e9724d9d36271d8b3491cf1b424e1e0bfd8a7
Joao Lisboa <joaoclisboa@ufmg.br>
2025-11-30 17:03:56 -0300

    criado o shapefile com os dados de MG

--- geoprocessamento/extractStates.py
+++ geoprocessamento/extractStates.py
@@ -1,1 +1,19 @@
 #Codigo que usa a biblioteca geopandas para extrair de um shapefile com todos os municipios do Brasil apenas um estado
+
+import geopandas as gpd
+from pathlib import Path
+
+ESTADO = "MG"
+
+def main(args=None):
+    base = Path(__file__).resolve().parent
+    caminho_shape = base / "shapefile" / "municipios.shp"
+
+    gdf = gpd.read_file(caminho_shape)
+    print(f"Lido: {caminho_shape} | feições: {len(gdf)} | CRS: {gdf.crs}")
+    municipios_filtrados = gdf[gdf["SIGLA"] == ESTADO]
+    print(f"lido em {ESTADO}| feições: {len(municipios_filtrados)} | CRS: {municipios_filtrados.crs}")
+    municipios_filtrados.to_file(f"municipios_{ESTADO}.shp")
+
+if __name__ == "__main__":
+    main()
